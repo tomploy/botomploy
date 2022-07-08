33d56fda19a28b43c45290f9c7b78389de34106c
Telma Blanc <thomas.blanc@mamoru.io*>
2022-07-09 00:26:52 +0200

    add env variable for discord token

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,8 +1,9 @@
 import { Client, ClientOptions } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
+require('dotenv').config();
 
-const token = "OTk1MDYyNDU0Mzc3Nzc1MTE1.Gu0mag.Xr1j9kxAXlzPRqSQ5-v8U0VpfC6kL5MICCg24c"; // add your token here
+const disc_tok = process.env.DISCORD_TOKEN; // add your token here
 
 console.log("Bot is starting...");
 
@@ -13,6 +14,6 @@ const client = new Client({
 ready(client);
 interactionCreate(client);
 
-client.login(token);
+client.login(disc_tok);
 
 //	https://discord.com/api/oauth2/authorize?client_id=995062454377775115&permissions=146029177920&scope=bot%20applications.commands
