ca288c6f4bfa80cb8922894a8dc45a9f31510e48
Telma Blanc <thomas.blanc@mamoru.io*>
2022-09-28 21:17:20 +0200

    clean project

--- src/utils/message.ts
+++
@@ -1,7 +0,0 @@
-import { CacheType, Client, CommandInteraction, Message, TextChannel } from "discord.js";
-import { APIMessage } from "discord-api-types/v9";
-
-export async function getMessage(client: Client, interaction: CommandInteraction<CacheType>, message: Message<boolean>|APIMessage): Promise<Message<boolean>> {
-    const channel = await client.channels.fetch(interaction.channelId) as TextChannel;
-    return await channel.messages.fetch(message.id);
-}
