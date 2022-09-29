d4eca99b514633a9790b0f96446df6c73cbaaeec
Telma Blanc <thomas.blanc@mamoru.io*>
2022-09-29 20:19:14 +0200

    js project

---
+++ src/commands/event.js
@@ -0,0 +1,8 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName("event")
+        .setDescription("manage collective events"),
+        .
+}
