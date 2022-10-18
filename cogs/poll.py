4dd2138d09412faae32835d72a2430490e6d1092
Telma Blanc <thomas.blanc@mamoru.io*>
2022-10-19 01:32:03 +0200

    format embed + manage reaction

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,109 +1,127 @@
 import os
-from pydoc import describe
 import sys
-from unicodedata import name
 from dotenv import load_dotenv
 
 import discord
-from discord import OptionChoice, option, Option
+from discord import OptionChoice, PartialEmoji, option, Option
 from discord.ext import commands
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from utils.emojis import numbers_emojis, hearts_emojis, circles_emojis
+from utils.emojis import emojis_db
 
 load_dotenv()
-
-
 class Poll(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.embed = ""
-        self.emoji_arr = []
+        self.polls = []
         super().__init__()
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if(self.bot.user.id == payload.user_id):
             return
+        
         guild = self.bot.get_guild(payload.guild_id);
-        member = guild.get_member(payload.user_id);
         channel = guild.get_channel(payload.channel_id);
         msg = channel.get_partial_message(payload.message_id)
 
+        poll = next(poll for poll in self.polls if poll.message.id == payload.message_id)
 
-        field = self.embed.fields[self.emoji_arr.index(payload.emoji)]
-        if(field.value == "Personne"):
-            field.value = payload.member.name
-        else:
-            field.value += "\n" + payload.member.name
-        
-        await msg.edit(embeds=[self.embed])
+        poll.add_member(payload.emoji, payload.user_id)
+        await msg.edit(embed=poll.embed)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         if(self.bot.user.id == payload.user_id):
             return
+
         guild = self.bot.get_guild(payload.guild_id);
-        member = guild.get_member(payload.user_id);
         channel = guild.get_channel(payload.channel_id);
         msg = channel.get_partial_message(payload.message_id)
 
-        field = self.embed.fields[self.emoji_arr.index(payload.emoji)]
-        start = field.value.find(member.name)
-        stop = start + len(member.name)
-        if len(field.value) > stop :
-            field.value = field.value[0: start:] + field.value[stop + 1::]
+        poll = next(poll for poll in self.polls if poll.message.id == payload.message_id)
+        poll.remove_member(payload.emoji, payload.user_id)
 
-        await msg.edit(embeds=[self.embed])
+        await msg.edit(embed=poll.embed)
 
     @commands.slash_command(guild_ids=[os.getenv("SERVER_ID")])
     @option("title", description="Titre du sondage")
     @option("items", description='items du sondage (virgule en separateur)')
     @option("desc", description="description du sondage", default="")
-    async def poll(self, ctx: discord.ApplicationContext, title: str, items: str, desc: str, bullet: Option(str, "test", choices=[
+    async def poll(self, ctx: discord.ApplicationContext, 
+        title: str, 
+        items: str, 
+        desc: str, 
+        emojis: Option(str, "emojis", choices=[
             OptionChoice(name="1️⃣ Nombres", value="numbers"),
             OptionChoice(name="❤️ Coeurs", value="hearts"),
             OptionChoice(name="🟣 Ronds", value="circles"),
         ], default="circles")):
 
-        if bullet == "numbers":
-            self.emoji_arr = numbers_emojis;
-        elif bullet == "hearts":
-            self.emoji_arr = hearts_emojis;
-        elif bullet == "circles":
-            self.emoji_arr = circles_emojis;
+        poll = PollData(
+            title,
+            items.split(','),
+            emojis,
+            desc
+        )
 
-        items_arr = items.split(',')
+        self.polls.append(poll)
 
-        embed = discord.Embed(
+        sondage = await ctx.response.send_message(embed=poll.embed)
+        message = await sondage.original_response()
+        await poll.add_reactions(message)
+
+
+empty_choice = "n o b o d y"
+class PollData():
+    def __init__(self, title, items, emojis, desc = ""):
+        self.message = None;
+        self.choices = []
+        self.embed = discord.Embed(
             title=title,
             color=discord.Color.green(),
             description=desc
         )
+        self.emojis = emojis_db[emojis]
 
-        for i in range(len(items_arr)):
-            embed.add_field(name=str(self.emoji_arr[i]) + " " + items_arr[i], value="Personne", inline=True)
-            # content +=  + "\n"
+        for i in range(len(items)):
+            item_dict = {
+                "item": items[i],
+                "members": []
+            }
+            self.choices.append(item_dict)
+            self.embed.add_field(name=str(self.emojis[i]) + " " + self.choices[i]["item"], value=empty_choice, inline=True)
 
-        # embed.add_field(name=EMOJI_UTIL.get("crossmark") + " Indispo", value="Personne", inline=True)
-        # content += 
+    async def add_reactions(self, message):
+        self.message = message
+        for i in range(len(self.choices)):
+            await self.message.add_reaction(self.emojis[i])
+    
+    def add_member(self, emoji, member):
+        index = self.emojis.index(emoji)
+        field = self.embed.fields[index]
+        self.choices[index]["members"].append(member)
 
-        self.embed = embed
+        value = ""
+        for member in self.choices[index]["members"]:
+            value += f"<@!{member}>\n"
+            # value += ">>> <@!{}>\n".format(member)
+        field.value = value;
 
-        sondage = await ctx.response.send_message(embeds=[embed])
+    def remove_member(self, emoji, member):
+        index = self.emojis.index(emoji)
+        field = self.embed.fields[index]
+        self.choices[index]["members"].remove(member)
 
+        value = ""
+        for member in self.choices[index]["members"]:
+            value += f"<@!{member}>\n"
 
-        message = await sondage.original_response();
-
-        for i in range(len(items_arr)):
-            await message.add_reaction(self.emoji_arr[i])
-
-        # await message.add_reaction(EMOJI_UTIL.get("crossmark"))
-        # await wait_for_reactions(ctx, sondage, client, props)
-
+        field.value = value if len(self.choices[index]["members"]) > 0 else empty_choice;
+        
 
 def setup(bot):
     bot.add_cog(Poll(bot))
